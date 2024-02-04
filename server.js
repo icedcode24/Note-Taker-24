c1d7b80767692b57f61ba1e4c4e491d93a96648a
cedric hamilton <cedrichamilton@cedrics-MBP.lan>
2024-02-04 11:23:23 -0500

    added server js

---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies
+const express = require('express');
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
+
+// Initialize express app
+const app = express();
+
+// Set up Port
+const Port = process.env.PORT || 3001;
+
+// Setting up static piblic with express
+app.use(express.static('public'));
+
+// Setting up Express for data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+
+// Setup listener
+app.listen(PORT, () =>
+console.log('Listening at http://localhost:${PORT}')
+);
